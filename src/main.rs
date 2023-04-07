cbc6dabe01ab2ee4ac21ed230454ee7e9b07858b
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-04-07 17:12:27 +0200

    fix: wait for input on error

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use playzer::{
     format::{self, PlaylistReaderWriter},
 };
 
-use std::{env, process};
+use std::{env, process, io};
 
 fn main() {
    
     let args = env::args();
     let config = config::Config::new(args).unwrap_or_else(|err| {
-        println!("{err}");
+        println!("{err}. Press any 'Enter' to quit.");
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
         process::exit(1)
     });
     let reader_writer: Box<dyn PlaylistReaderWriter> = match &config.format() {
@@ -17,7 +19,9 @@ fn main() {
     };
     let v = reader_writer.parse_file(&config);
     reader_writer.write_file(&v, &config).unwrap_or_else(|err| {
-        println!("{err}");
+        println!("{err}. Press any 'Enter' to quit.");
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
         process::exit(1)
     })
 }
