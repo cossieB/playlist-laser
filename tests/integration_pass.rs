3cc9b1fbc7961bb89f17a0c7ad9369eae0cc6e7a
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-04-09 16:40:04 +0200

    integration tests

---
+++ tests/integration_pass.rs
@@ -0,0 +1,19 @@
+use std::path;
+
+use playzer::*;
+
+#[test]
+fn it_works() {
+    let args = ["".to_string(),"./test_assets/test.pls".to_string(), "asx".to_string()].into_iter();
+    let config = config::Config::new(args).unwrap_or_else(|err| {
+        panic!("{err}")
+    });
+    let reader = format::get_reader_writer(&config.format());
+    let writer = format::get_reader_writer(&config.output_format());
+    let v = reader.parse_file(&config);
+    let path = writer.write_file(&v, &config).unwrap_or_else(|err| {
+        panic!("{err}")
+    });
+    let path = path::Path::new(&path);
+    assert!(path.exists());
+}
